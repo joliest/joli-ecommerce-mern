f847b0c7dafdb018686f0540d26a47c072fb24a5
Joliver Estampador <joliverestampador@gmail.com>
2021-02-06 11:19:21 +0800

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <>
+        <h1>Welcome to E commerce Site</h1>
+      </>
+    </div>
+  );
+}
+
+export default App;
