82e94ecb04d15c36790c6b4ce6d683e1229d1fc8
Joliver Estampador <joliverestampador@gmail.com>
2021-02-09 22:27:29 +0800

    adding reducer, actions and local storage userInfo

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,18 +12,29 @@ import {
     productDetailsReducer 
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer
 })
 
+const cartItemsFromLocal = localStorage.getItem('cartItems') 
+    ? JSON.parse(localStorage.getItem('cartItems')) 
+    : []
 
-const cartItemsFromLocal = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const userInfoFomLocal = localStorage.getItem('userInfo') 
+    ? JSON.parse(localStorage.getItem('userInfo')) 
+    : null
+
 const initialState = {
     cart: {
         cartItems: cartItemsFromLocal
+    },
+    userLogin: {
+        userInfo: userInfoFomLocal
     }
 }
 
