09650eea430ac49d2c5d1b2d888f35bfe5ba445b
Joliver Estampador <joliver.estampador@tooltwist.com>
2021-04-04 12:06:10 +0800

    adding logout functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, NavDropdown, Nav, Container, FormControl } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../actions/userActions'
 
 const Header = () => {
+    const dispatch = useDispatch();
+
+    const userLogin = useSelector(state =>  state.userLogin);
+    const { userInfo } = userLogin;
+
+    const logoutHandler = () => {
+        dispatch(logout)
+    }
+
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -16,9 +27,22 @@ const Header = () => {
                             <LinkContainer to="/cart">
                                 <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to="/login">
-                                <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
-                            </LinkContainer>
+                            {
+                                userInfo ? (
+                                    <NavDropdown title={userInfo.name} id="username">
+                                        <LinkContainer to="/profile">
+                                            <NavDropdown.Item>Profile</NavDropdown.Item>
+                                        </LinkContainer>
+                                        <NavDropdown.Item onClick={logoutHandler}>
+                                            Logout
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                ) : (
+                                    <LinkContainer to="/login">
+                                        <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
+                                    </LinkContainer>
+                                )
+                            }
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
